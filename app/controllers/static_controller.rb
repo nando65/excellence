37ea1b4be500f961c876e6fd52b5fa2b4e69d47a
Fernando <nando65@hotmail.com>
2018-01-05 21:06:13 -0800

    second commit

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,20 @@
+class StaticController < ApplicationController
+
+  def homepage
+  end
+
+  def research
+  end
+
+  def design
+  end
+
+  def engineering
+  end
+
+  def analytics
+  end
+
+  def marketing
+  end
+end
